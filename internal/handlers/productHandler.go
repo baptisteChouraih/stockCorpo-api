2733af3ed399a6f78b04371561a165281ced25cd
baptisteChouraih <Baptiste.Chouraih@uphf.fr>
2025-10-28 15:31:10 +0100

    FEAT(productHandler) Creation du handler pour la creation des produits

---
+++ internal/handlers/productHandler.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"net/http"
+	"stockCorpo-api/internal/models"
+	"stockCorpo-api/internal/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+type ProductHandler struct {
+	productService *services.ProductService
+}
+
+// NewProductHandler : cree une nouvelle instance de ProductHandler tqt, je sais ce que je fais
+func NewProductHandler(serv *services.ProductService) *ProductHandler {
+	return &ProductHandler{productService: serv}
+}
+
+func (h *ProductHandler) CreateProduct(c *gin.Context) {
+	var product models.Product
+	if err := c.ShouldBindJSON(&product); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := h.productService.CreateProduct(c.Request.Context(), &product); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"Message": "Product created successfully!"})
+}
