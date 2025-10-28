7c10f669649c156984ff0133434540092b8c70b9
baptisteChouraih <Baptiste.Chouraih@uphf.fr>
2025-10-28 15:31:34 +0100

    FEAT(productService) Creation du Service pour la creation des produits

---
+++ internal/services/productService.go
@@ -0,0 +1,37 @@
+package services
+
+import (
+	"errors"
+	"stockCorpo-api/internal/models"
+	"stockCorpo-api/internal/repositories"
+
+	"context"
+)
+
+type ProductService struct {
+	productRepo *repositories.ProductRepository
+}
+
+func NewProductService(productRepository *repositories.ProductRepository) *ProductService {
+	return &ProductService{productRepo: productRepository}
+}
+
+func (p *ProductService) CreateProduct(ctx context.Context, product *models.Product) error {
+	if product.Types == "" {
+		return errors.New("types is required")
+	}
+
+	if product.Price == nil {
+		return errors.New("price is required")
+	}
+
+	if *product.Price <= 0 {
+		return errors.New("price must be greater than 0")
+	}
+
+	if product.Stock == nil {
+		return errors.New("stock is required")
+	}
+
+	return p.productRepo.Create(ctx, product)
+}
