43df3089b2edc011b97f395d0b36b19008d498ab
baptisteChouraih <Baptiste.Chouraih@uphf.fr>
2025-10-28 15:32:47 +0100

    FIX(Product) ajout de nullable

--- internal/models/product.go
+++ internal/models/product.go
@@ -1,8 +1,8 @@
 package models
 
 type Product struct {
-	IdProduct int     `json:"idProduct"`
-	Types     string  `json:"types"`
-	Price     float64 `json:"price"`
-	Stock     bool    `json:"stock"`
+	IdProduct int      `json:"idProduct"`
+	Types     string   `json:"types"`
+	Price     *float64 `json:"price"`
+	Stock     *bool    `json:"stock"`
 }
